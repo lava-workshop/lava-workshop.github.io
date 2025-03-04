9f02bcba5c0b9a269719bc3e28ddca85640bd039
vominhduc <vmduc.khtn@gmail.com>
2025-03-04 22:24:03 +0900

    Update workshop.jsx

--- src/app/components/workshop.jsx
+++ src/app/components/workshop.jsx
@@ -1,10 +1,9 @@
 const Workshop = () => {
     return (
         <div className="text-justify">
-            <span className="font-bold text-[1.3em]">The Workshop has been successfully held. We are thankful to all organizers, authors, reviews, challenge participants, invited speakers and attendees. We look forward to your attendance next time.</span>
-            <br/> <br/> <br/>
+            
            <p>
-            There has been significant advancement in AI and machine learning, particularly with the development of large language models like GPT recently. These models have demonstrated their prowess in harnessing extensive text data to achieve remarkable performance across various natural language understanding tasks. As a result, there is a growing interest in expanding these capabilities to encompass additional modalities such as images, audios, and videos, giving rise to large vision-language models (LVLMs) like GPT-V. LAVA workshop aims to unleash the full potential of research in large vision-language models (LVLMs) by emphasizing the convergence of diverse modalities, including text, images, and video. Furthermore, the workshop provides a platform for delving into the practical applications of LVLMs across a broad spectrum of domains, such as healthcare, education, entertainment, transportation, finance, etc.
+               We are delighted to organize the Second Workshop on Large Vision—Language Model Learning and Applications (LAVA 2025) and The Large Vision—Language Model Learning and Applications Grand Challenge (LAVA Challenge) in conjunction with ACM MM 2025. Please refer to the corresponding page for more details. We look forward to your contribution. See you soon in Dublin, Ireland.
             </p>
 
 

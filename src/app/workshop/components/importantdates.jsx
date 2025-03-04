8f0b61b69f62b75f9e1db5f805bf6967546611b8
vominhduc <vmduc.khtn@gmail.com>
2025-03-04 21:39:48 +0900

    Update importantdates.jsx

--- src/app/workshop/components/importantdates.jsx
+++ src/app/workshop/components/importantdates.jsx
@@ -3,9 +3,9 @@ const ImportantDates = () => {
         
         <ul className="list-disc list-inside space-y-2 mt-2 ml-4">
             <li>Paper submission deadline: <span className="font-bold">tbd</span></li>
-            <li>Acceptance notification: <span className="font-bold">tbd</span></li>
-            <li>Camera-ready deadline: <span className="font-bold">tbd</span></li>
-            <li>Workshop date: <span className="font-bold">tbd</span></li>
+            <li>Acceptance notification: <span className="font-bold">2025/7/24</span></li>
+            <li>Camera-ready: <span className="font-bold">2025/8/3</span></li>
+            <li>Workshop date: <span className="font-bold">2025/10/27-28</span></li>
         </ul>
     </div>
 }

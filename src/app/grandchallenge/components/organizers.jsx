9981360fc6cbded4ce3d3c3b645bb455370f87b8
vominhduc <vmduc.khtn@gmail.com>
2025-03-04 22:57:30 +0900

    Update organizers.jsx

--- src/app/grandchallenge/components/organizers.jsx
+++ src/app/grandchallenge/components/organizers.jsx
@@ -5,13 +5,12 @@ const Organizers = ()=>{
          
         <div className="grid sm:grid-cols-4 grid-cols-2 mt-4 gap-y-6">
             <PersonAvatar name="Duc Minh Vo" image="/vmduc.jpg" affiliation={"University of Tokyo, Japan"} />
-            <PersonAvatar name="Huy H. Nguyen" image="/nhhuy.jpg" affiliation={"National Institute of Informatics, Japan"} />
-            <PersonAvatar name="Trung-Nghia Le" image="/ltnghia.jpg" affiliation={"University of Science, Vietnam"} />
             <PersonAvatar name="Akihiro Sugimoto" image="/sugimoto.jpg" affiliation={"National Institute of Informatics, Japan"} />
             <PersonAvatar name="Hideki Nakayama" image="/nakayama.jpg" affiliation={"University of Tokyo, Japan"} />
-            <PersonAvatar name="Minh-Triet Tran" image="/tmtriet.png" affiliation={"University of Science, Vietnam"} />
             <PersonAvatar name="Khan Md Anwarus Salam" image="/khan_salam.jpg" affiliation={"SoftBank, Japan"} />
-         
+            <PersonAvatar name="Jiaxuan Li" image="/avatar.jpg" affiliation={"University of Tokyo, Japan"} />
+            <PersonAvatar name="Junwen Mo" image="/avatar.jpg" affiliation={"University of Tokyo, Japan"} />
+            <PersonAvatar name="Daichi Sato" image="/Daichi.jpg" affiliation={"University of Tokyo, Japan"} />
         </div>
 
         <br/>

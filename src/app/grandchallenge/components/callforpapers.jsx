83fab2ffe4eaa976a61c1aeb870e0cf0a0bc034d
vominhduc <vmduc.khtn@gmail.com>
2025-03-04 22:47:19 +0900

    Update callforpapers.jsx

--- src/app/grandchallenge/components/callforpapers.jsx
+++ src/app/grandchallenge/components/callforpapers.jsx
@@ -1,7 +1,7 @@
 const CallForPapers = () => {
     return (
         <div className="text-justify">
-            
+            {/*            
             <p className="text-pretty mb-2">
             We welcome people to submit papers about large vision-language models (LVLMs) to LAVA workshop. Accepted papers will be presented in our workshop and will be published in the ACCV workshop proceeding. <span className="font-bold">We accept short papers (non-archived) which are up to 7 pages in ACCV format, excluding references; and long papers (archived) which are up to 14 pages in ACCV format, excluding references.</span> Submission policies adhere to the ACCV submission policies.
             </p>
@@ -20,7 +20,7 @@ const CallForPapers = () => {
                 <li>Applications of LVLMs</li>
                 <li>LVLMs and other modalities</li>
                 <li>LVLMs for low resources</li>
-            </ul>
+            </ul> */}
 {/*             <span className="font-bold text-[1.3em]"><a href='https://cmt3.research.microsoft.com/LAVA2024' className="underline text-[1.1em] text-[#232ba1]">Submit your paper here</a></span>. */}
                 
 

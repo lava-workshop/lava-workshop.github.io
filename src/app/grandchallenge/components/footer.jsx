47598689350a7c338bdda7cd1f2b15e8832d1039
vominhduc <vmduc.khtn@gmail.com>
2025-03-04 22:59:58 +0900

    Update footer.jsx

--- src/app/grandchallenge/components/footer.jsx
+++ src/app/grandchallenge/components/footer.jsx
@@ -10,13 +10,7 @@ const Footer = () => {
                     <Image src="/NII.jpg" alt="NII" width={100} height={50} className="sm:h-[5em] h-[5em]" />
                 </div>
                 <div className="flex flex-col justify-center">
-                    <Image src="/hcmus-logo-white.png" alt="footer" width={150} height={50} className="sm:w-[7em] w-[4em]"/>
-                </div>
-                <div className="flex flex-col justify-center">
                     <Image src="/BeyondAI.jpg" alt="Beyond AI" width={150} height={50} className="sm:w-[7em] w-[4em]"/>
-                </div>
-                <div className="flex flex-col justify-center">
-                    <Image src="/TASUKI.png" alt="TASUKI" width={150} height={50} className="sm:w-[7em] w-[4em]"/>
                 </div>
                 {/* <div className="flex flex-col justify-center">
                     <Image src="/softbank.png" alt="Soft Bank" width={200} height={200} className="sm:w-[13em] w-[6em]" />

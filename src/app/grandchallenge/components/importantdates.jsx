643a90b99d2920c4401ad2c79bb842b5acdfb6b4
vominhduc <vmduc.khtn@gmail.com>
2025-03-08 13:01:33 +0900

    Update importantdates.jsx

--- src/app/grandchallenge/components/importantdates.jsx
+++ src/app/grandchallenge/components/importantdates.jsx
@@ -2,9 +2,10 @@ const ImportantDates = () => {
     return <div className="text-justify">
         
         <ul className="list-disc list-inside space-y-2 mt-2 ml-4">
-            <li>Registration opened: <span className="font-bold">tbd</span></li>
-            <li>Public data released:  <span className="font-bold">tbd</span></li>
-            <li>Registration closed: <span className="font-bold">tbd</span></li>
+            <li>Registration opened: <span className="font-bold">2025/3/15</span></li>
+            <li>Public data released:  <span className="font-bold">2025/4/15</span></li>
+            <li>Registration closed: <span className="font-bold">2025/5/31</span></li>
+            <li>Private data released: <span className="font-bold">2025/6/23 (to be updated)</span></li>
             <li>Results, Report, Paper submission deadline: <span className="font-bold">2025/6/30</span></li>
             <li>Notification: <span className="font-bold">2025/7/24</span></li>
             <li>Camera-ready submission: <span className="font-bold">2025/8/26</span></li>

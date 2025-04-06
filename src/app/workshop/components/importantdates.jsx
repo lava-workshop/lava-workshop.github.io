daf4913168edea1f9bae0937a670b07aa2da71ca
vominhduc <vmduc.khtn@gmail.com>
2025-04-06 21:26:08 +0900

    Update importantdates.jsx

--- src/app/workshop/components/importantdates.jsx
+++ src/app/workshop/components/importantdates.jsx
@@ -5,7 +5,7 @@ const ImportantDates = () => {
             <li>Paper submission deadline: <span className="font-bold">2025/6/15</span></li>
             <li>ACM MM fast track submision: <span className="font-bold">2025/7/10</span></li>
             <li>Acceptance notification: <span className="font-bold">2025/7/24</span></li>
-            <li>Camera-ready: <span className="font-bold">2025/8/3</span></li>
+            <li>Camera-ready: <span className="font-bold">2025/7/31</span></li>
             <li>Workshop date: <span className="font-bold">2025/10/27-28</span></li>
         </ul>
     </div>

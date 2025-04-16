45819079ab246792e3bf4b8177f19d3529777eac
vominhduc <vmduc.khtn@gmail.com>
2025-04-16 23:20:34 +0900

    Update organizers.jsx

--- src/app/workshop/components/organizers.jsx
+++ src/app/workshop/components/organizers.jsx
@@ -4,8 +4,8 @@ const Organizers = ()=>{
     return <div className="text-justify"> 
          
         <div className="grid sm:grid-cols-4 grid-cols-2 mt-4 gap-y-6">
-            <PersonAvatar name="Duc Minh Vo" image="/vmduc.jpg" affiliation={"University of Tokyo, Japan"} />
-            <PersonAvatar name="Huy H. Nguyen" image="/nhhuy.jpg" affiliation={"National Institute of Informatics, Japan"} />
+            <PersonAvatar name="Duc Minh Vo" image="/vmduc.jpg" affiliation={"SB Intuitions, Japan"} />
+            <PersonAvatar name="Huy H. Nguyen" image="/nhhuy.jpg" affiliation={"SB Intuitions, Japan"} />
             <PersonAvatar name="Trung-Nghia Le" image="/ltnghia.jpg" affiliation={"University of Science, Vietnam"} />
             <PersonAvatar name="Akihiro Sugimoto" image="/sugimoto.jpg" affiliation={"National Institute of Informatics, Japan"} />
             <PersonAvatar name="Hideki Nakayama" image="/nakayama.jpg" affiliation={"University of Tokyo, Japan"} />

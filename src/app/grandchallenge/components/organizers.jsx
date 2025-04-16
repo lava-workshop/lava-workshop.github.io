691d3f7f8031d2114f301f15bac44ed4765ba010
vominhduc <vmduc.khtn@gmail.com>
2025-04-16 23:21:34 +0900

    Update organizers.jsx

--- src/app/grandchallenge/components/organizers.jsx
+++ src/app/grandchallenge/components/organizers.jsx
@@ -4,7 +4,7 @@ const Organizers = ()=>{
     return <div className="text-justify"> 
          
         <div className="grid sm:grid-cols-4 grid-cols-2 mt-4 gap-y-6">
-            <PersonAvatar name="Duc Minh Vo" image="/vmduc.jpg" affiliation={"University of Tokyo, Japan"} />
+            <PersonAvatar name="Duc Minh Vo" image="/vmduc.jpg" affiliation={"SB Intuitions, Japan"} />
             <PersonAvatar name="Akihiro Sugimoto" image="/sugimoto.jpg" affiliation={"National Institute of Informatics, Japan"} />
             <PersonAvatar name="Hideki Nakayama" image="/nakayama.jpg" affiliation={"University of Tokyo, Japan"} />
             <PersonAvatar name="Khan Md Anwarus Salam" image="/khan_salam.jpg" affiliation={"SoftBank, Japan"} />

34771658e2744d1c4a281627e214ba31a5be1cb8
vominhduc <vmduc.khtn@gmail.com>
2025-04-17 10:13:11 +0900

    Update organizers.jsx

--- src/app/grandchallenge/components/organizers.jsx
+++ src/app/grandchallenge/components/organizers.jsx
@@ -5,10 +5,10 @@ const Organizers = ()=>{
          
         <div className="grid sm:grid-cols-4 grid-cols-2 mt-4 gap-y-6">
             <PersonAvatar name="Duc Minh Vo" image="/vmduc.jpg" affiliation={"SB Intuitions, Japan"} />
-            <PersonAvatar name="Akihiro Sugimoto" image="/sugimoto.jpg" affiliation={"National Institute of Informatics, Japan"} />
+{/*             <PersonAvatar name="Akihiro Sugimoto" image="/sugimoto.jpg" affiliation={"National Institute of Informatics, Japan"} /> */}
             <PersonAvatar name="Hideki Nakayama" image="/nakayama.jpg" affiliation={"University of Tokyo, Japan"} />
             <PersonAvatar name="Khan Md Anwarus Salam" image="/khan_salam.jpg" affiliation={"SoftBank, Japan"} />
-            <PersonAvatar name="Jiaxuan Li" image="/avatar.jpg" affiliation={"University of Tokyo, Japan"} />
+            <PersonAvatar name="Jiaxuan Li" image="/avatar.jpg" affiliation={"SB Intuitions, Japan"} />
             <PersonAvatar name="Junwen Mo" image="/avatar.jpg" affiliation={"University of Tokyo, Japan"} />
             <PersonAvatar name="Daichi Sato" image="/Daichi.jpg" affiliation={"University of Tokyo, Japan"} />
         </div>

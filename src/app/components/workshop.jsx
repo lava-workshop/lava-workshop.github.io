42203711646a7c998ba75d27ae704f759ebe4904
Huy H. Nguyen <honghuy127@gmail.com>
2025-04-28 11:30:00 +0900

    Update workshop.jsx

--- src/app/components/workshop.jsx
+++ src/app/components/workshop.jsx
@@ -1,12 +1,9 @@
 const Workshop = () => {
     return (
         <div className="text-justify">
-            
-           <p>
-               We are delighted to organize the Second Workshop on Large Vision—Language Model Learning and Applications (LAVA 2025) and The Large Vision—Language Model Learning and Applications Grand Challenge (LAVA Challenge) in conjunction with ACM MM 2025. Please refer to the corresponding page for more details. We look forward to your contribution. See you soon in Dublin, Ireland.
+            <p>
+                We are delighted to organize the <a href="https://lava-workshop.github.io/workshop" target="_blank" rel="noopener noreferrer">Second Workshop on Large Vision—Language Model Learning and Applications (LAVA 2025)</a> and <a href="https://lava-workshop.github.io/grandchallenge" target="_blank" rel="noopener noreferrer">The Large Vision—Language Model Learning and Applications Grand Challenge (LAVA Challenge)</a> in conjunction with ACM MM 2025. Please refer to the corresponding page for more details. We look forward to your contribution. See you soon in Dublin, Ireland.
             </p>
-
-
         </div>
     )
 }

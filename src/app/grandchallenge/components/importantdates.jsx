1345e4ebdc3e39a958c3950332dae4505982b7f4
vominhduc <vmduc.khtn@gmail.com>
2025-06-15 18:33:17 +0900

    Update importantdates.jsx

--- src/app/grandchallenge/components/importantdates.jsx
+++ src/app/grandchallenge/components/importantdates.jsx
@@ -7,7 +7,9 @@ const ImportantDates = () => {
                     <span className="font-bold"> 2025/4/17</span></li>
             <li>Registration closed: <span className="font-bold">2025/5/31</span></li>
             <li><span style={{textDecoration: 'line-through'}}>Private data released: 2025/6/23 (to be updated)</span> We decided to use the test data for public and private leaderboard</li>
-            <li>Results, Report, Paper submission deadline: <span className="font-bold">2025/6/30</span></li>
+            <li><span style={{textDecoration: 'line-through'}}>Results, Report, Paper submission deadline: <span className="font-bold">2025/6/30</span></span></li>
+            <li>Code and short report submision: <span className="font-bold">2025/7/1</span></li>
+            <li>Paper submission (on invitation): <span className="font-bold">2025/7/30</span></li>
             <li>Notification: <span className="font-bold">2025/7/24</span></li>
             <li>Camera-ready submission: <span className="font-bold">2025/8/26</span></li>
             <li>Grand challenge date: <span className="font-bold">2025/10/27 - 31</span></li>

978870d2cee4029195a9678705497adb75e99ded
vominhduc <vmduc.khtn@gmail.com>
2025-06-29 14:07:25 +0900

    Update importantdates.jsx

--- src/app/workshop/components/importantdates.jsx
+++ src/app/workshop/components/importantdates.jsx
@@ -2,8 +2,8 @@ const ImportantDates = () => {
     return <div className="text-justify">
         
         <ul className="list-disc list-inside space-y-2 mt-2 ml-4">
-            <li>Paper submission deadline: <span className="font-bold">2025/6/15</span></li>
-            <li>ACM MM fast track submision: <span className="font-bold">2025/7/11</span></li>
+            <li>Paper submission deadline: <span style={{textDecoration: 'line-through'}}>2025/6/15</span><span className="font-bold">2025/7/11 (Extended)</span></li>
+            <li>ACM MM fast track submision: <span className="font-bold">2025/7/11</span></li> For fast track, please include main conference reviews, your response and meta review in your submission.
             <li>Acceptance notification: <span className="font-bold">2025/8/1</span></li>
             <li>Camera-ready: <span className="font-bold">2025/8/11</span></li>
             <li>Workshop date: <span className="font-bold">2025/10/27-28</span></li>

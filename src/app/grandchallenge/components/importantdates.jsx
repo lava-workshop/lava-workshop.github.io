ce88f93464ac3e183c5e05f7a9c6e290d7fb603d
vominhduc <vmduc.khtn@gmail.com>
2025-08-25 18:01:32 +0900

    Update importantdates.jsx

--- src/app/grandchallenge/components/importantdates.jsx
+++ src/app/grandchallenge/components/importantdates.jsx
@@ -2,15 +2,15 @@ const ImportantDates = () => {
     return <div className="text-justify">
         
         <ul className="list-disc list-inside space-y-2 mt-2 ml-4">
-            <li>Registration opened: <span className="font-bold">2025/3/15</span></li>
+            <li>Registration opened: <span style={{textDecoration: 'line-through'}}>2025/3/15</span></li>
             <li>Data released:  <span style={{textDecoration: 'line-through'}}>2025/4/15</span>
                     <span className="font-bold"> 2025/4/17</span></li>
-            <li>Registration closed: <span className="font-bold">2025/5/31</span></li>
-            <li><span style={{textDecoration: 'line-through'}}>Private data released: 2025/6/23 (to be updated)</span> We decided to use the test data for public and private leaderboard</li>
+            <li>Registration closed: <span style={{textDecoration: 'line-through'}}>2025/5/31</span></li>
+            <li><span style={{textDecoration: 'line-through'}}>Private data released: 2025/6/23 (to be updated) We decided to use the test data for public and private leaderboard</span></li>
             <li><span style={{textDecoration: 'line-through'}}>Results, Report, Paper submission deadline: <span className="font-bold">2025/6/30</span></span></li>
-            <li>Code and short report submision: <span className="font-bold">2025/7/1</span></li>
-            <li>Paper submission (on invitation): <span className="font-bold">2025/7/30</span></li>
-            <li>Notification: <span className="font-bold">2025/7/24</span></li>
+            <li>Code and short report submision: <span style={{textDecoration: 'line-through'}}>2025/7/1</span></li>
+            <li>Paper submission (on invitation): <span style={{textDecoration: 'line-through'}}>2025/7/30</span></li>
+            <li>Notification: <span style={{textDecoration: 'line-through'}}>2025/7/24</span></li>
             <li>Camera-ready submission: <span className="font-bold">2025/8/26</span></li>
             <li>Grand challenge date: <span className="font-bold">2025/10/27 - 31</span></li>
         </ul>

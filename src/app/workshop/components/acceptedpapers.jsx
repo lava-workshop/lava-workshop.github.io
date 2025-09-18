1005e990798a52ed00aacefe800cf1da7f92c384
vominhduc <vmduc.khtn@gmail.com>
2025-09-19 01:48:58 +0900

    Update acceptedpapers.jsx

--- src/app/workshop/components/acceptedpapers.jsx
+++ src/app/workshop/components/acceptedpapers.jsx
@@ -2,19 +2,22 @@ const AcceptedPapers = () => {
     return (
         <div className="text-justify">
             
-            <span className="font-bold text-[1.3em]">Workshop track</span>
+            <span className="font-bold text-[1.3em]">Workshop Proceedings</span>
             <ol className="list-disc list-inside space-y-2 mt-2 mb-8 ml-4">
-                <li>Duc-Tuan Luu, Viet-Tuan Le, Duc Minh Vo: Questioning, Answering, and Captioning for Zero-Shot Detailed Image Caption</li>
-                <li>Rento Yamaguchi, Kenji Yanai: Exploring Cross-Attention Maps in Multi-modal Diffusion Transformers for Training-Free Semantic Segmentation</li>
-                <li>Viet-Tham Huynh, Trong-Thuan Nguyen, Thao Thi Phuong Dao, Tam V. Nguyen, Minh-Triet Tran: DermAI: A Chatbot Assistant for Skin lesion Diagnosis Using Vision and Large Language Models</li>
-                <li>Felix Hsieh, Huy Hong Nguyen, April Pyone Maung Maung, Dmitrii Usynin, Isao Echizen: Mitigating Backdoor Attacks using Activation-Guided Model Editing</li>
+                <li>Jiadong Yan, Quan Zhang, Yifan Zhou, Tianle Yang, Ke Zhang: Few-shot Anomaly Detection based on Long Short Text Interactive Contrastive Learning</li>
+                <li>Anwar Dilawar Shaikh, Janak Kapuriya, Arnav Goel, Medha Hira, Apoorv Singh, Jay Saraf, Sanjana Sanjeev, Vaibhav Nauriyal, Avinash Anand, Zhengkui Wang, Rajiv Ratn Shah: Enhancing Scientific Visual Question Answering via Vision-Caption aware Supervised Fine-Tuning</li>
+                <li>Tun-Yuan Chang, Kenneth Chandra, Cheng-Hsin Hsu: Harvesting Temporal Correlation in Large Vision-Language Models: Using Pose Estimation as a Case Study</li>
             </ol>
-            <span className="font-bold text-[1.3em]">Challenge track</span>
+            <span className="font-bold text-[1.3em]">Fast Track</span>
             <ol start="5" className="list-disc list-inside space-y-2 mt-2 mb-8 ml-4">
-                <li>(1st prize) Thanh-Son Nguyen, Viet-Tham Huynh, Van-Loc Nguyen, Minh-Triet Tran: An Approach to Complex Visual Data Interpretation with Vision-Language Models</li>
-                <li>(2nd prize) Gia-Nghia Tran, Duc-Tuan Luu, Dang-Van Thin: Exploring Visual Multiple-Choice Question Answering with Pre-trained Vision-Language Models</li>
-                <li>(3rd prize) Trong Hieu Nguyen Mau, Binh Truc Nhu Nguyen, Vinh Nhu Hoang, Minh-Triet Tran, Hai-Dang Nguyen: Enhancing Visual Question Answering with Pre-trained Vision-Language Models: An Ensemble Approach at the LAVA Challenge 2024</li>
-            </ol>               
+                <li>Phuoc-Nguyen Bui, Khanh-Binh Nguyen, Hyunseung Choo: Accelerating Conditional Prompt Learning via Masked Image Modeling for Vision-Language Models</li>
+            </ol> 
+            <span className="font-bold text-[1.3em]">Fast Track</span>
+            <ol start="5" className="list-disc list-inside space-y-2 mt-2 mb-8 ml-4">
+                <li>Song-Li Wu: LAMDA: Leveraging Multi-Scale and Dynamic Alignment for Robust Referring Video Object Segmentation</li>
+                <li>Jun, Kexin Lv, An Guo: Hierarchical Temporal Views for Policy Optimization in Multimodal Video Reasoning</li>
+                <li>Song-Li Wu: Bridging the Modal Gap: A Targeted Patch Refinement and Residual Preservation Framework for Efficient Referring Expression Segmentation</li>
+            </ol>  
 
         </div>
     )

954b28c62b7a8afe25108f8c50993f6b6bec2c3c
vominhduc <vmduc.khtn@gmail.com>
2025-09-23 01:04:08 +0900

    Update acceptedpapers.jsx

--- src/app/grandchallenge/components/acceptedpapers.jsx
+++ src/app/grandchallenge/components/acceptedpapers.jsx
@@ -2,11 +2,11 @@ const AcceptedPapers = () => {
     return (
         <div className="text-justify">
             <ul className="list-disc list-inside space-y-2 mt-2 ml-4">
-                <li>Daichi Sato, Duc Minh Vo, Khan Md. Anwarus Salam, Hidenori Shoji, Yuma Matsuoka, Takara Taniguchi, Kaito Baba, Hideki Nakayama: LAVA Challenge 2025: Advancing Japanese Document Understanding with a Challenging PDF-VQA Benchmark (Challenge Summary Paper)</li>
-                <li>TBD </li>
-                <li>TBD </li>
-                <li>TBD </li>
-                <li>TBD </li>
+                <li>(Summary paper) Daichi Sato, Duc Minh Vo, Khan Md. Anwarus Salam, Hidenori Shoji, Yuma Matsuoka, Takara Taniguchi, Kaito Baba, Hideki Nakayama: LAVA Challenge 2025: Advancing Japanese Document Understanding with a Challenging PDF-VQA Benchmark (Challenge Summary Paper)</li>
+                <li>(1st Prize, SYSUpporter team) Longfeng Chen, Zheng Xiao, Juyuan Wang, Zeyu Huang, Yawen Zeng, Jin Xu: HEAR: A Holistic Extraction and Agentic Reasoning Framework for Document Understanding</li>
+                <li>(2nd Prize, Woof team) Haoxuan Li, Wei Song, Aofan Liu, Peiwu Qin: AdaDocVQA: Adaptive Framework for Long Document Visual Question Answering in Low-Resource Settings</li>
+                <li>(3rd Prize, nsbsk team) Ao Zhou, Zebo Gu, Tenghao S, Jiawen , Mingsheng , Zifeng , Yafeng , Zhiwei , Qing Gu: Hierarchical Vision-Language Reasoning for Multimodal Multiple-Choice Question Answering</li>
+                <li>(4th Prize, char team) Mizuki Yamano, Keito , Hisashi Miyamori: Two-Stage Approach Using a Pretrained Language Model for Question Answering on Japanese Document Images</li>
             </ul>              
 
         </div>

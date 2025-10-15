11b1b1439fad75145c63d93f5d5736196f3d5423
vominhduc <vmduc.khtn@gmail.com>
2025-10-15 09:14:53 +0900

    Update schedule.jsx

--- src/app/workshop/components/schedule.jsx
+++ src/app/workshop/components/schedule.jsx
@@ -1,24 +1,27 @@
 const Schedule = () => {
     return <div className="text-justify">
+        LOCATION: Distillers, Hyatt
         <span className="font-bold text-[1.3em]">Morning Session</span>
         <ul className="list-disc list-inside space-y-2 mt-2 ml-4">
-            <li> <span className="font-bold">--:--</span> - Opening Remark</li>
-            <li><span className="font-bold">--:--</span> - Keynote Talk: Dr. Md. Mamunur Rashid (The King Abdulaziz Center for World Culture - Ithra) Cross-Modal Trust: Evaluating LVLMs for Safeguarding Health Information</li>
-            <li><span className="font-bold">--:--</span> - Janak Kapuriya: Enhancing Scientific Visual Question Answering via Vision-Caption aware Supervised Fine-Tuning</li>
-            <li><span className="font-bold">--:--</span> - Jiadong Yan (online): Few-shot Anomaly Detection based on Long Short Text Interactive Contrastive Learning</li>
-            <li><span className="font-bold">--:--</span> - Tun-Yuan Chang: Harvesting Temporal Correlation in Large Vision-Language Models: Using Pose Estimation as a Case Study</li>
-            <li><span className="font-bold">--:--</span> - Nam Nguyen Xuan (online): StructCon-ST: Connectivity-Aware Spatio-Temporal Fine-Grained Image Analysis</li>
-            <li><span className="font-bold">--:--</span> - Jun Wan (pre-recorded video): Hierarchical Temporal Views for Policy Optimization in Multimodal Video Reasoning</li>
+            <li> <span className="font-bold">09:00</span> - Opening Remark</li>
+            <li><span className="font-bold">09:05</span> - Keynote Talk: Dr. Md. Mamunur Rashid (The King Abdulaziz Center for World Culture - Ithra) Cross-Modal Trust: Evaluating LVLMs for Safeguarding Health Information</li>
+            <li><span className="font-bold">10:00</span> - Janak Kapuriya (online): Enhancing Scientific Visual Question Answering via Vision-Caption aware Supervised Fine-Tuning</li>
+            <li><span className="font-bold">10:15</span> - Jiadong Yan (online): Few-shot Anomaly Detection based on Long Short Text Interactive Contrastive Learning</li>
+            <li>10:30~11:00 Coffee break</li>
+            <li><span className="font-bold">11:00</span> - Tun-Yuan Chang: Harvesting Temporal Correlation in Large Vision-Language Models: Using Pose Estimation as a Case Study</li>
+            <li><span className="font-bold">11:15</span> - Nam Nguyen Xuan (online): StructCon-ST: Connectivity-Aware Spatio-Temporal Fine-Grained Image Analysis</li>
+            <li><span className="font-bold">11:30</span> - Jun Wan (pre-recorded video): Hierarchical Temporal Views for Policy Optimization in Multimodal Video Reasoning</li>
         </ul>
 
         <span className="font-bold text-[1.3em]">Afternoon Session</span>
         <ul className="list-disc list-inside space-y-2 mt-2 ml-4">
-            <li><span className="font-bold">--:--</span> - Keynote Talk: Dr. Seitaro Shinagawa (SB Intuitions, online) Sarashina2-Vision: Toward Vision -- Language Models for Understanding Japanese Figures and Conceptual/Explanatory Diagrams</li>
-            <li><span className="font-bold">--:--</span> - Daichi Sato: LAVA Grand Challenge Introduction</li>
-            <li><span className="font-bold">--:--</span> - SYSUpporter team: HEAR: A Holistic Extraction and Agentic Reasoning Framework for Document Understanding</li>
-            <li><span className="font-bold">--:--</span> - Woof team: AdaDocVQA: Adaptive Framework for Long Document Visual Question Answering in Low-Resource Settings</li>
-            <li><span className="font-bold">--:--</span> - nsbsk team: Hierarchical Vision-Language Reasoning for Multimodal Multiple-Choice Question Answering</li>
-            <li><span className="font-bold">--:--</span> - char team: Two-Stage Approach Using a Pretrained Language Model for Question Answering on Japanese Document Images</li>
+            <li><span className="font-bold">13:00</span> - Keynote Talk: Dr. Seitaro Shinagawa (SB Intuitions, online) Sarashina2-Vision: Toward Vision -- Language Models for Understanding Japanese Figures and Conceptual/Explanatory Diagrams</li>
+            <li><span className="font-bold">14:00</span> - Daichi Sato: LAVA Grand Challenge Introduction</li>
+            <li><span className="font-bold">14:15</span> - SYSUpporter team: HEAR: A Holistic Extraction and Agentic Reasoning Framework for Document Understanding</li>
+            <li>14:30~15:00 Coffee break</li>
+            <li><span className="font-bold">15:00</span> - Woof team: AdaDocVQA: Adaptive Framework for Long Document Visual Question Answering in Low-Resource Settings</li>
+            <li><span className="font-bold">15:15</span> - nsbsk team: Hierarchical Vision-Language Reasoning for Multimodal Multiple-Choice Question Answering</li>
+            <li><span className="font-bold">15:30</span> - char team: Two-Stage Approach Using a Pretrained Language Model for Question Answering on Japanese Document Images</li>
         </ul>
     </div>
 }
